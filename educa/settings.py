7a9ea541ca9e6f0716158064a8da9e6196f1d550
mario-ak37 <alfredkipkogei@proton.me>
2026-01-22 21:14:16 +0300

    create an authentication system for the CMS

--- educa/settings.py
+++ educa/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = "educa.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -71,6 +71,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = "educa.wsgi.application"
 
@@ -121,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
