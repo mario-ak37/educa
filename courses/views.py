e498c3e4e99449be5e81c6db19f417a8357dd0f7
mario-ak37 <alfredkipkogei@proton.me>
2026-01-26 22:53:32 +0300

    add content to course modules

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,6 @@
+from django.apps import apps
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.forms.models import modelform_factory
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import ListView
@@ -6,7 +8,7 @@ from django.views.generic.base import TemplateResponseMixin, View
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from .forms import ModuleFormSet
-from .models import Course
+from .models import Content, Course, Module
 
 
 # Create your views here.
@@ -73,3 +75,30 @@ class CourseModuleUpdateView(TemplateResponseMixin, View):
             formset.save()
             return redirect("manage_course_list")
         return self.render_to_response({"course": self.course, "formset": formset})
+
+class ContentCreateUpdateView(TemplateResponseMixin, View):
+    module = None
+    model = None
+    obj = None
+    template_name = 'courses/manage/content/form.html'
+    def get_model(self, model_name):
+            if model_name in ['text', 'video', 'image', 'file']:
+                return apps.get_model(
+                    app_label='courses', model_name=model_name
+                )
+            return None
+    def get_form(self, model, *args, **kwargs):
+            Form = modelform_factory(
+                model, exclude=['owner', 'order', 'created', 'updated']
+            )
+            return Form(*args, **kwargs)
+    def dispatch(self, request, module_id, model_name, id=None):
+         self.module = get_object_or_404(
+            Module, id=module_id, course__owner=request.user
+        )
+         self.model = self.get_model(model_name)
+        if id:
+         self.obj = get_object_or_404(
+                self.model, id=id, owner=request.user
+            )
+         return super().dispatch(request, module_id, model_name, id)
